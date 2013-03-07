0c28a84bbf56b6141e95af48e56dd1b04dfdc02e
Marko Heijnen <info@markoheijnen.com>
2013-03-07 14:55:32 +0100

    Add selectbox to sidebar by adding it to the first widget. Not a great solution but it is the best

--- horizontal-sidebar.php
+++ horizontal-sidebar.php
@@ -34,6 +34,12 @@ function horizontal_sidebar_example() {
 class Horizontal_Sidebar {
 	public static $end_row = '<div class="clearfix"></div>';
 	private $sidebars = array();
+	private $dropdown_showed = array();
+
+	function __construct() {
+		if( is_admin() )
+			add_filter( 'dynamic_sidebar_params', array( $this, 'add_selectbox' ), 10000 );
+	}
 
 	public function register( $sidebar_id, $columns ) {
 		global $wp_registered_sidebars, $_wp_sidebars_widgets;
@@ -49,6 +55,58 @@ class Horizontal_Sidebar {
 		}
 
 		return false;
+	}
+
+
+	public function add_selectbox( $params ) {
+		global $wp_registered_sidebars;
+
+		$screen = get_current_screen();
+
+		if( 'widgets' == $screen->base ) {
+			$sidebar_id = $params[0]['id'];
+
+			if( is_admin() && ! isset( $this->dropdown_showed[ $sidebar_id ] ) && count( $this->sidebars[ $sidebar_id ] ) > 1 ) {
+				echo $this->show_selectbox( $sidebar_id, $this->sidebars[ $sidebar_id ] );
+
+				$this->dropdown_showed[ $sidebar_id ] = true;
+			}
+		}
+
+		return $params;
+	}
+
+	private function show_selectbox( $sidebar_id, $columns ) {
+		$return  = '<div class="sidebar-description" style="margin-top:-10px;">' . __( 'Amount of columns', 'horizontal_sidebar' ) . ' &nbsp; <select class="sidebar-columns">';
+
+		$default_amount = $columns[0];
+		$current_amount = $this->get_columns_for_sidebar( $sidebar_id, $default_amount );
+		sort( $columns );
+
+		foreach( $columns as $column ) {
+			if( $column == $default_amount )
+				$column_name = $column . ' (' . __( 'default', 'horizontal_sidebar' ) . ')';
+			else
+				$column_name = $column;
+
+			if( $column == $current_amount )
+				$return .= '<option value="' . $column . '" selected="selected">' . $column_name . '</option>';
+			else
+				$return .= '<option value="' . $column . '">' . $column_name . '</option>';
+		}
+
+		$return .= '</select></div>';
+
+		return $return;
+	}
+
+	private function get_columns_for_sidebar( $sidebar_id, $default ) {
+		$options = get_option( 'horizontal_sidebar_columns', array() );
+
+		if( isset( $options[ $sidebar_id ] ) )
+			return absint( $options[ $sidebar_id ] );
+
+		return $default;
 	}
 }
 
